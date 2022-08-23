f93bed1e166917a865c804975f5884e21f057a04
saiful142348 <saiful17182103353@gmail.com>
2022-08-23 18:48:19 +0600

    contact all ok

--- app/Http/Controllers/Contactcontroller.php
+++ app/Http/Controllers/Contactcontroller.php
@@ -6,12 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ContactFormMail;
+use Brian2694\Toastr\Facades\Toastr;
 
 class Contactcontroller extends Controller
 {
-   public function store(){
+    public function store()
+    {
 
-    $contact_from_data= request()->all();
-    Mail::to('saiful17182103353@gmail.com')->send(new ContactFormMail(  $contact_from_data));
-   }
+        $contact_from_data = request()->all();
+        Mail::to('saiful17182103353@gmail.com')->send(new ContactFormMail($contact_from_data));
+        Toastr::success('your Message successfully ', 'success', [
+            "positionClass" => "toast-top-right", "closeButton"
+            =>
+            "true", "progressBar" => "true"
+        ]);
+        return redirect()->back();
+    }
 }
